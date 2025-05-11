2a6368c2d76a8bcbbf65133d23a0766554abf76e
Mohini Agarwal <160155298+AgarwalMohini@users.noreply.github.com>
2025-05-11 09:56:32 +0530

    changes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Button } from 'antd';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App p-5">
-      <h1>APPOINT EASE</h1>
-      <Button type="primary">Primary Button</Button>
-=======
-<<<<<<< HEAD
-    <div className="App">
-      <h1>APPOINT EASE</h1>
-=======
-    <div className="App p-5">
-      <h1>APPOINT EASE</h1>
-      <Button type="primary">Primary Button</Button>
->>>>>>> 6c6ad74254b2068712e25d521e4b64d91a758e24
->>>>>>> 2c9ea4a1b992054d813e41ed5ebc114d15fd8705
-    </div>
+    <BrowserRouter>
+    
+    <Routes>
+
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+    </Routes>
+    
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,14 @@
+import { Divider } from 'antd'
 import React from 'react'
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='authentication'>
+      <div className='authentication-form card p-2'>
+        <h1 className='card-title'>Nice To meet you</h1>
+      </div>
+    </div>
   )
 }
 
-export default Register
+export default Register;
